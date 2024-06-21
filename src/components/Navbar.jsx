9cfcf9bfdfcf33ea8d8275fa8a423653a9b4b064
Your Name <dannydotdev@gmail.com>
2024-06-21 17:57:12 +0100

    folder structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { appleImg } from "../utils";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="images" width={14} height={18} />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
