0c253ad290727466e065d9a307d94055e96cfef8
Your Name <dannydotdev@gmail.com>
2024-06-22 14:08:45 +0100

    Video carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import { hightlightsSlides } from "../constants";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const VideoCarousel = () => {
   const videoRef = useRef([]);
   const videoSpanRef = useRef([]);
@@ -50,6 +53,13 @@ const VideoCarousel = () => {
                   playsInline={true}
                   preload="auto"
                   muted
+                  ref={(element) => (videoRef.current[ind] = element)}
+                  onPlay={() => {
+                    setVideo((prevVideo) => ({
+                      ...prevVideo,
+                      isPlaying: true,
+                    }));
+                  }}
                   //   autoPlay
                 >
                   <source src={list.video} type="video/mp4" />
@@ -65,6 +75,23 @@ const VideoCarousel = () => {
             </div>
           </div>
         ))}
+      </div>
+
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 rounded-full backdrop-blur">
+          {videoRef.current.map((_, i) => (
+            <span
+              key={i}
+              ref={(element) => (videoDivRef.current[i] = element)}
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+            >
+              <span
+                className="h-full absolute w-full rounded-full"
+                ref={(element) => (videoSpanRef.current[i] = element)}
+              ></span>
+            </span>
+          ))}
+        </div>
       </div>
     </>
   );
