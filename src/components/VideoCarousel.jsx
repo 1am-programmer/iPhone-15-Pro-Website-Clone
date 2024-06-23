6ae118dfcb4280cd686b8775507216dd01115846
Your Name <dannydotdev@gmail.com>
2024-06-23 18:47:54 +0100

    pause and play animation on the video carouel section of our highlights page

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -3,6 +3,7 @@ import { hightlightsSlides } from "../constants";
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { pauseImg, playImg, replayImg } from "../utils";
 
 const VideoCarousel = () => {
   const videoRef = useRef([]);
@@ -40,6 +41,21 @@ const VideoCarousel = () => {
       });
     }
   }, [videoId, startPlay]);
+
+  const handleProcess = (type, i) => {
+    switch (type) {
+      case "video-end":
+        setVideo((prevVideo) => ({
+          ...prevVideo,
+          isEnd: true,
+          videoId: i + 1,
+        }));
+        break;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <>
@@ -92,6 +108,20 @@ const VideoCarousel = () => {
             </span>
           ))}
         </div>
+
+        <button className="control-btn">
+          <img
+            src={isLastVideo ? replayImg : !isPlaying ? playImg : pauseImg}
+            alt={isLastVideo ? "replay" : !isPlaying ? "play" : "pause"}
+            onClick={
+              isLastVideo
+                ? () => handleProcess("video-reset")
+                : !isPlaying
+                ? () => handleProcess("play")
+                : () => handleProcess("pause")
+            }
+          />
+        </button>
       </div>
     </>
   );
