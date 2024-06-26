5a6696e6def31edd695d3b4d5253c20140f3612d
Danny <dannydotdev@gmail.com>
2024-06-26 11:18:15 +0100

    iPhone size variation

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants";
+import { models, sizes } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -84,18 +84,32 @@ const Model = () => {
 
             <div className="flex-center">
               <ul className="color-container">
-                {models.map((item,i)=>(
-                <li key={i} className="h-6 w-6 rounded-full mx-2  cursor-pointer"
-                style={{
-                  backgroundColor:item.color[0]
-                }}
-                onClick={()=>setModel(item)}
-                />
-              ))}</ul>
-
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="h-6 w-6 rounded-full mx-2  cursor-pointer"
+                    style={{
+                      backgroundColor: item.color[0],
+                    }}
+                    onClick={() => setModel(item)}
+                  />
+                ))}
+              </ul>
 
               <button className="size-btn-container">
-
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={()=>setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
               </button>
             </div>
           </div>
