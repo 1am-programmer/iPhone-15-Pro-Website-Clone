79a29b1188827d20122157877d79a2a953bc3ba5
Danny <dannydotdev@gmail.com>
2024-07-18 10:32:50 +0100

    Packages&build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "1am-programmer",
-    project: "javascript-nextjs"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "1am-programmer",
+      project: "javascript-nextjs",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
